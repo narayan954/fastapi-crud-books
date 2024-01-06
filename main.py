802742efa87a1f9d48d84fc70059d532e9c5ba49
Narayan soni <narayansoni854@gmail.com>
2024-01-06 23:22:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from dotenv import dotenv_values
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+
+from routes import router as book_router
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to MongoDB")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+    print("Disconnected from MongoDB")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+app.include_router(book_router, tags=["books"], prefix="/book")
